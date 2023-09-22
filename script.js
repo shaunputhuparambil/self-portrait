5cd8d36a550d668050cdd857cd204dbc4783e862
shaunputhuparambil <142811028+shaunputhuparambil@users.noreply.github.com>
2023-09-22 23:24:06 +0000

    h2

--- script.js
+++ script.js
@@ -10,21 +10,25 @@ const firebaseConfig = {
 };
 const app = firebase.initializeApp(firebaseConfig);
 const analytics = firebase.analytics(app);
+
+// References to Firestore and Storage
 const db = firebase.firestore();
+const storage = firebase.storage().ref();
 
 // Function to retrieve data from Firestore and display it
 function fetchData() {
     const contentContainer = document.getElementById('contentContainer');
-    db.collection("uploads").get().then((querySnapshot) => {
+    db.collection("uploads").orderBy('timestamp', 'desc').get().then((querySnapshot) => {
+        contentContainer.innerHTML = ""; // clear current content
         querySnapshot.forEach((doc) => {
             let data = doc.data();
             let div = document.createElement('div');
             div.className = "content";
 
-            if (data.fileType.startsWith("image/")) {
-                div.innerHTML = `<img src="${data.fileUrl}" alt="${data.fileName}"><p>${data.fileName}<br><span class="uploader">Uploaded by: ${data.uploaderName}</span></p>`;
-            } else if (data.fileType === "application/pdf") {
-                div.innerHTML = `<iframe src="${data.fileUrl}"></iframe><p>${data.fileName}<br><span class="uploader">Uploaded by: ${data.uploaderName}</span></p>`;
+            if (data.fileName.endsWith(".jpg") || data.fileName.endsWith(".png")) {
+                div.innerHTML = `<img src="${data.fileURL}" alt="${data.fileName}"><p>${data.fileName}<br><span class="uploader">Uploaded by: ${data.uploaderName}</span></p>`;
+            } else if (data.fileName.endsWith(".pdf")) {
+                div.innerHTML = `<iframe src="${data.fileURL}"></iframe><p>${data.fileName}<br><span class="uploader">Uploaded by: ${data.uploaderName}</span></p>`;
             }
 
             contentContainer.appendChild(div);
@@ -46,34 +50,32 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('fileInput').click();
     });
 
-    document.getElementById('fileInput').addEventListener('change', function(event) {
+    document.getElementById('fileInput').addEventListener('change', async function(event) {
         const uploaderName = document.getElementById('uploaderName').value.trim();
         const files = event.target.files;
 
         for(let i = 0; i < files.length; i++) {
             let file = files[i];
-            let fileReader = new FileReader();
 
-            fileReader.onload = function(e) {
-                db.collection("uploads").add({
-                    fileName: file.name,
-                    fileType: file.type,
-                    fileUrl: e.target.result,
-                    uploaderName: uploaderName
-                })
-                .then((docRef) => {
-                    fetchData();
-                })
-                .catch((error) => {
-                    console.error("Error adding document: ", error);
-                });
-            };
+            // Store file in Firebase Storage
+            let storageRef = storage.child('uploads/' + file.name);
+            await storageRef.put(file);
+            
+            let downloadURL = await storageRef.getDownloadURL();
 
-            fileReader.readAsDataURL(file);
+            // Save the file data in Firestore
+            await db.collection('uploads').add({
+                fileName: file.name,
+                fileURL: downloadURL,
+                uploaderName: uploaderName,
+                timestamp: firebase.firestore.FieldValue.serverTimestamp()
+            });
         }
 
         // Clear the input for repeated use
         event.target.value = "";
         document.getElementById('uploaderName').value = "";
+        
+        fetchData(); // Refresh displayed content
     });
 });
